47f9e3ed2120a84db330be833b0a2e99baeb5739
Benjamin Boortz <benjamin.boortz@mailbox.org>
2018-09-02 08:21:54 +0100

    cats updated

---
+++ cat_images/main.py
@@ -0,0 +1,27 @@
+"""A big "thank you" to all our Cats who made this year's badge possible!"""
+
+___name___         = "Cats"
+___license___      = "MIT"
+___dependencies___ = ["wifi", "http", "ugfx_helper", "sleep", "app"]
+___categories___   = ["EMF"]
+___bootstrapped___ = True
+
+import ugfx_helper, os, wifi, ugfx, http, time, sleep, app
+from tilda import Buttons
+
+ugfx_helper.init()
+ugfx.clear()
+
+ugfx.text(5, 5, "Loading...", ugfx.BLACK)
+try:
+    image = http.get("http://s3.amazonaws.com/tilda-badge/sponsors/screen.png").raise_for_status().content
+    ugfx.display_image(0,0,bytearray(image))
+except:
+    ugfx.clear()
+    ugfx.text(5, 5, "Couldn't download sponsors", ugfx.BLACK)
+
+while (not Buttons.is_pressed(Buttons.BTN_A)) and (not Buttons.is_pressed(Buttons.BTN_B)) and (not Buttons.is_pressed(Buttons.BTN_Menu)):
+    sleep.wfi()
+
+ugfx.clear()
+app.restart_to_default()
